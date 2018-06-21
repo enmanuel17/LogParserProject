2a14b0284828e10565ab6ef2980c5bb62e0c28fe
vagrant <vagrant@vagrant.vm>
2018-06-21 04:33:31 +0000

    fixing line breaks in SQL queries

--- dbParselog.py
+++ dbParselog.py
@@ -19,39 +19,41 @@ def query_runner(query):
 def main():
 
     # Stores 3 SQL queiries to answer the report questions.
-    most_popular_articles = \
-            "select title as article, count(*) as views \
-            from log join articles \
-            on log.path like concat('%',articles.slug) \
-            group by title \
-            order by views \
-            desc \
-            limit 3;"
-    most_popular_authors = \
-            "select name,sum(views) as total_views \
-            from \
-                (select title as article,author, count(*) as views \
-                from log join articles \
-                on log.path like concat('%',articles.slug) \
-                group by title,author) \
-                as author_views \
-            join authors \
-            on author = authors.id \
-            group by name \
-            order by total_views \
-            desc;"
-    one_percent_errors = \
-            "select date, cast(error_percentage as int) from \
-            (select date,(cast(errors as float) / cast(total_requests as float) * 100) as error_percentage \
-            from (select date,total_requests,errors \
-            from (select dates.date,count(*) as errors \
-            from log join dates \
-            on cast(time as date) = dates.date \
-            where status != '200 OK' \
-            group by dates.date,status order by date) \
-            as errors join (select cast(time as date),count(*) \
-            as total_requests from log group by cast(time as date)) as requests on errors.date = requests.time) as subq) \
-            as error_percentages where error_percentage > 1;"
+    most_popular_articles = """
+    select title as article, count(*) as views
+    from log join articles
+    on log.path like concat('%',articles.slug)
+    group by title
+    order by views
+    desc
+    limit 3;"""
+    most_popular_authors = """
+    select name,sum(views) as total_views
+    from
+        (select title as article,author, count(*) as views
+        from log join articles
+        on log.path like concat('%',articles.slug)
+        group by title,author)
+        as author_views
+    join authors
+    on author = authors.id
+    group by name
+    order by total_views
+    desc;"""
+    one_percent_errors = """
+    select date, cast(error_percentage as int) from
+    (select date,(cast(errors as float) / cast(total_requests as float) * 100)
+    as error_percentage
+    from (select date,total_requests,errors
+    from (select dates.date,count(*) as errors
+    from log join dates
+    on cast(time as date) = dates.date
+    where status != '200 OK'
+    group by dates.date,status order by date)
+    as errors join (select cast(time as date),count(*)
+    as total_requests from log group by cast(time as date)) as requests
+    on errors.date = requests.time) as subq)
+    as error_percentages where error_percentage > 1;"""
 
     # prints  the response from each query in a nice format.
     result1 = query_runner(most_popular_articles)
